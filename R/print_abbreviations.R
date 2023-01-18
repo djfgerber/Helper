ac84d23cdf42dbcfab7c6244701f2e64d509d2b9
Timothée Rondez <rondti@swisstph.ch>
2023-01-18 14:37:27 +0100

    add a testfunction to test github

---
+++ R/print_abbreviations.R
@@ -0,0 +1,11 @@
+
+
+print_abbreviations <- function(name) {
+  if (name == 'xiawei') {
+    return('xw')
+  } else if (name == 'timothee') {
+    return('tr')
+  } else if (name == 'daniel') {
+    return('djfg')
+  }
+}
